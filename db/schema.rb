81788fa42fb010e0007ef99703a801aa047576d9
Mahesh Mesta <mahesh8mesta@gmail.com>
2017-07-26 16:21:56 +0530

    minor error

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170726082426) do
+ActiveRecord::Schema.define(version: 20170726102102) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "api_api_status_lists", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "urls", force: :cascade do |t|
     t.text     "url"
